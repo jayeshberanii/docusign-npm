b8e8f9ebda04971890642eaf1969e176b8cea772
jayeshberani <jayesh.berani@aspiresoftserv.com>
2023-09-21 11:25:02 +0530

    Updates

---
+++ precache-manifest.75afa698125b6a05c4d0fe3b3f8dd285.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f77deb7b1ba3273d495025be68606413",
+    "url": "./index.html"
+  },
+  {
+    "revision": "afbeb29392e5318ac225",
+    "url": "./static/css/main.b693b373.chunk.css"
+  },
+  {
+    "revision": "8d966335e4ee0b420040",
+    "url": "./static/js/2.9736e554.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.9736e554.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "afbeb29392e5318ac225",
+    "url": "./static/js/main.ec5daf83.chunk.js"
+  },
+  {
+    "revision": "3b14e14bc065e020908e",
+    "url": "./static/js/runtime-main.365c1ee8.js"
+  }
+]);
